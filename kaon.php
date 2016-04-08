c6d116ac9f55c8d107775fb3ea672875b70dbb3e
Ricky <rickymillang@gmail.com>
2016-04-08 09:24:09 +0800

    Added kaon file

---
+++ kaon.php
@@ -0,0 +1,124 @@
+<ul class="nav right-actions"><li class="me dropdown session js-session open" data-global-action="t1me" id="user-dropdown">
+        <a href="/settings/account" class="btn js-tooltip settings dropdown-toggle js-dropdown-toggle" id="user-dropdown-toggle" data-placement="bottom" role="button" aria-haspopup="true" data-original-title="Profile and settings"><img class="avatar size32" src="https://pbs.twimg.com/profile_images/638486591887925248/q2AmCbO3_normal.jpg" alt="Profile and settings" data-user-id="79979502"></a>
+        <div class="dropdown-menu"><div class="js-first-tabstop" tabindex="0"></div>
+    <div class="dropdown-caret">
+      <span class="caret-outer"></span>
+      <span class="caret-inner"></span>
+    </div>
+    <ul tabindex="-1" role="menu" aria-labelledby="user-dropdown-toggle" aria-hidden="false">
+      
+      <li class="current-user" data-name="profile" role="presentation">
+        
+
+<a href="/ahhyet" class="account-summary account-summary-small js-nav" data-nav="view_profile" role="menuitem"><div class="content"><div class="account-group js-mini-current-user" data-user-id="79979502" data-screen-name="ahhyet"><b class="fullname">clarisse jenn</b><span class="screen-name hidden" dir="ltr">@ahhyet</span><small class="metadata">View profile</small></div></div></a>
+
+      </li>
+
+      <li class="dropdown-divider" role="presentation"></li>
+      
+      <li data-name="lists" role="presentation"><a href="/ahhyet/lists" data-nav="all_lists" role="menuitem">Lists</a></li>
+      <li class="dropdown-divider" role="presentation"></li>
+
+      
+
+      
+      <li role="presentation"><a href="//support.twitter.com" data-nav="help_center" role="menuitem">Help</a></li>
+
+        
+        <li class="js-keyboard-shortcut-trigger" data-nav="shortcuts" role="presentation">
+          <button type="button" class="dropdown-link" role="menuitem">Keyboard shortcuts</button>
+        </li>
+
+      
+        <li role="presentation"><a href="https://ads.twitter.com/start?ref=gl-tw-tw-twitter-ads" target="_blank" data-nav="ads" role="menuitem">Twitter Ads</a></li>
+
+      
+        <li role="presentation"><a class="user-dropdown-analytics" href="https://analytics.twitter.com/" target="_blank" data-nav="user_dropdown_analytics" role="menuitem">Analytics</a></li>
+
+      
+
+      
+
+
+      <li class="dropdown-divider" role="presentation"></li>
+
+      
+      <li role="presentation"><a href="/settings/account" data-nav="settings" class="js-nav" role="menuitem">Settings</a></li>
+
+      
+      <li class="js-signout-button" id="signout-button" data-nav="logout" role="presentation">
+        <button type="button" class="dropdown-link" role="menuitem">Log out</button>
+        <form class="t1-form dropdown-link-form signout-form" id="signout-form" action="/logout" method="POST">
+          <input type="hidden" value="34184e795b21cd75791411e3c633fc72146222ab" name="authenticity_token" class="authenticity_token">
+          <input type="hidden" name="reliability_event" class="js-reliability-event">
+          <input type="hidden" name="scribe_log">
+        </form>
+      </li>
+
+    </ul>
+  <div class="js-last-tabstop" tabindex="0"></div></div>
+
+      </li><li role="complementary" aria-labelledby="global-new-tweet-button" class="topbar-tweet-btn">
+        <button id="global-new-tweet-button" type="button" class="js-global-new-tweet js-tooltip btn primary-btn tweet-btn js-dynamic-tooltip" data-placement="bottom" data-component-context="new_tweet_button">
+          <span class="Icon Icon--tweet Icon--large"></span>
+          <span class="text">Tweet</span>
+        </button>
+      </li></ul>
+
+      <div class="dropdown-menu"><div class="js-first-tabstop" tabindex="0"></div>
+    <div class="dropdown-caret">
+      <span class="caret-outer"></span>
+      <span class="caret-inner"></span>
+    </div>
+    <ul tabindex="-1" role="menu" aria-labelledby="user-dropdown-toggle" aria-hidden="false">
+      
+      <li class="current-user" data-name="profile" role="presentation">
+        
+
+<a href="/ahhyet" class="account-summary account-summary-small js-nav" data-nav="view_profile" role="menuitem"><div class="content"><div class="account-group js-mini-current-user" data-user-id="79979502" data-screen-name="ahhyet"><b class="fullname">clarisse jenn</b><span class="screen-name hidden" dir="ltr">@ahhyet</span><small class="metadata">View profile</small></div></div></a>
+
+      </li>
+
+      <li class="dropdown-divider" role="presentation"></li>
+      
+      <li data-name="lists" role="presentation"><a href="/ahhyet/lists" data-nav="all_lists" role="menuitem">Lists</a></li>
+      <li class="dropdown-divider" role="presentation"></li>
+
+      
+
+      
+      <li role="presentation"><a href="//support.twitter.com" data-nav="help_center" role="menuitem">Help</a></li>
+
+        
+        <li class="js-keyboard-shortcut-trigger" data-nav="shortcuts" role="presentation">
+          <button type="button" class="dropdown-link" role="menuitem">Keyboard shortcuts</button>
+        </li>
+
+      
+        <li role="presentation"><a href="https://ads.twitter.com/start?ref=gl-tw-tw-twitter-ads" target="_blank" data-nav="ads" role="menuitem">Twitter Ads</a></li>
+
+      
+        <li role="presentation"><a class="user-dropdown-analytics" href="https://analytics.twitter.com/" target="_blank" data-nav="user_dropdown_analytics" role="menuitem">Analytics</a></li>
+
+      
+
+      
+
+
+      <li class="dropdown-divider" role="presentation"></li>
+
+      
+      <li role="presentation"><a href="/settings/account" data-nav="settings" class="js-nav" role="menuitem">Settings</a></li>
+
+      
+      <li class="js-signout-button" id="signout-button" data-nav="logout" role="presentation">
+        <button type="button" class="dropdown-link" role="menuitem">Log out</button>
+        <form class="t1-form dropdown-link-form signout-form" id="signout-form" action="/logout" method="POST">
+          <input type="hidden" value="34184e795b21cd75791411e3c633fc72146222ab" name="authenticity_token" class="authenticity_token">
+          <input type="hidden" name="reliability_event" class="js-reliability-event">
+          <input type="hidden" name="scribe_log">
+        </form>
+      </li>
+
+    </ul>
+  <div class="js-last-tabstop" tabindex="0"></div></div>
